2b7a36c99c8368f5a02165c6df7a8ba94b64a40d
Bahula21 <bahulakbangera@gmail.com>
2026-09-20 10:18:50 +0530

    Set up tasks database

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import sqlite3
 
 app= Flask(__name__)
 
-@app.route("/")
+def get_db():
+    return sqlite3.connect("tracker.db")
+
+def init_db():
+    connection = get_db()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task TEXT,
+            subject TEXT,
+            priority INTEGER,
+            due_date DATE,
+            status INTEGER DEFAULT 1,
+            completed_at DATE
+        )
+    """)
+
+    connection.commit()
+    connection.close()
+
+@app.route("/", methods=["GET","POST"])
 def home():
+
+    if request.method == "POST":
+        task = request.form["task"]
+        subject = request.form["subject"]
+        priority = request.form["priority"]
+        due_date = request.form["due_date"]
+
     return render_template("index.html")
 
+    
+    
+
+init_db()
 
 if __name__ == "__main__":
     app.run(debug=True)
